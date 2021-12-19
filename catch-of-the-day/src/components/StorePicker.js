ddc494e0adfdd7bec8ca97e9b6b7ab6a9b0ceb7e
Ona <onushkis@gmail.com>
2021-12-19 21:41:07 +0100

    add hepler functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   render() {
     return (
@@ -7,7 +7,7 @@ class StorePicker extends React.Component {
         <form action="" className="store-selector">
           {/*comment */}
           <h2> Please Enter A Store </h2>
-          <input type="text" required placeholder="Store Name" />
+          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
           <button type="submit">Visit Store &#8594;</button>
         </form>
       </Fragment>

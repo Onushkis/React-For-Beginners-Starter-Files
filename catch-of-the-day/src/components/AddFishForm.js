9dcbf21be174fbb3e4502a68d21e857ae005733a
Ona <onushkis@gmail.com>
2021-12-20 00:05:27 +0100

    Add a AddFish componnet

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+    createFish = (event) =>{
+        // 1. Stop the form from the submiting
+        event.preventDefault();
+        console.log('Making a fish 🐟');
+};
+  render() {
+    return (
+   <form className="fish-edit" onSubmit={this.createFish}>
+        <input name ="name"  type ="text" placeholder="Name" />
+        <input name ="price" type ="text" placeholder="Price" />
+        <select name ="status">
+        <option value="available">Fresh!</option>
+        <option value="unvailable">Sold Out</option>
+        </select>
+        <textarea name ="desc" placeholder="Desc" />
+        <input name ="image" type ="text" placeholder="Image" />
+       <button type="submit">+ Add Fish</button>
+   </form>
+
+
+    );
+  }
+}
+
+export default AddFishForm;

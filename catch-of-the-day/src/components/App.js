de903e3aa5bb1996997b05a211eb79d7ac987d9d
Ona <onushkis@gmail.com>
2022-01-05 18:05:27 +0100

    Added changes loading data into state onClick

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,32 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+    state = {
+    fishes:{},
+    order:{}
+  };
+  addFish = (fish) => {
+   // 1. TAke a copy of the exsisting state
+   const fishes = {...this.state.fishes};
+   // 2.  Add our new fish to that fishs variable
+   fishes[`fish${Date.now()}`] = fish;
+   // 3. Set the new fishes object to state
+   this.setState({ fishes });
+  };
+  loadSampleFishes = () => {
+  this.setState({ fishes: sampleFishes });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"> </Header>
+          <Header tagline="Fresh Seafood Market" /> 
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
